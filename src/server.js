782f4c5dbd474d0a0310144d28a13a1e76fb58a1
Rafal Wilinski <raf.wilinski@gmail.com>
2018-04-06 22:53:28 +0200

    Add metric fetching basic mechanism

--- src/server.js
+++ src/server.js
@@ -2,21 +2,46 @@ import express from 'express';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import dotenv from 'dotenv';
+import { ServerStyleSheet } from 'styled-components';
+import AWS from 'aws-sdk';
+import { chunk } from 'lodash';
 import App from './client/App';
 import Html from './client/Html';
-import { ServerStyleSheet } from 'styled-components';
 
 dotenv.config();
 const port = 3000;
 const server = express();
 
-server.get('/', (req, res) => {
+const cloudwatch = new AWS.CloudWatch();
+const lambda = new AWS.Lambda();
+
+const getStatisticValues = () => {} // todo
+
+server.get('/', async (req, res) => {
+  const { Metrics } = await cloudwatch.listMetrics({
+    Namespace: 'AWS/Lambda'
+  }).promise();
+
+  const funcMetrics = {};
+
+  Metrics.forEach((metric) => {
+    if (metric.Dimensions[0] && metric.Dimensions[0].Name === 'FunctionName') { //TODO: Scan instead of peeking first el
+      const name = metric.Dimensions[0].FunctionName.Name;
+      if (!funcMetrics.hasOwnProperty(name)) {
+        funcMetrics[name] = {};
+      }
+      
+      funcMetrics[name][metric.MetricName] = getStatisticValues(metric.MetricName, metric.Dimensions[0]);
+    }
+  })
+
+  console.log(metrics);
+
   const sheet = new ServerStyleSheet();
-  const body = renderToString(sheet.collectStyles(<App />));
+  const body = renderToString(sheet.collectStyles(<App metrics={metrics} />));
   const styles = sheet.getStyleTags();
   const title = 'Lambda Dashboard';
   
-
   res.send(
     Html({
       body,
