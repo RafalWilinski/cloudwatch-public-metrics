7bbb4c385e5ebe24a88cf34e5c80d612232a790a
Rafal Wilinski <raf.wilinski@gmail.com>
2018-05-03 13:06:00 +0200

    Remove unnecessary imports from Error.js

--- src/client/Error.js
+++ src/client/Error.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import { VictoryChart, VictoryLine, VictoryTheme } from "victory";
 
 const ErrorContainer = styled.div`
   display: flex;
