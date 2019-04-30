985d9d8b96d8c76ee65d5cce8a71a0225434dd64
Rafal Wilinski <raf.wilinski@gmail.com>
2019-04-30 10:59:21 +0200

    Add index page

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/index.css";
+
+function Index() {
+  return (
+    <div className="homePage">
+      <div>Welcome to next.js serverless ⚡</div>
+    </div>
+  );
+}
+
+export default Index;
