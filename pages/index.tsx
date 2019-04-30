e485f348e5aa5a5004b44799c71749a9a9fbd278
Rafal Wilinski <raf.wilinski@gmail.com>
2019-04-30 12:15:16 +0200

    Remove v1 frontend

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,42 +3,44 @@ import { VictoryChart, VictoryLine, VictoryAxis } from "victory";
 
 import theme from "./theme";
 import "./styles/index.css";
-
 import getMetrics from "../getMetrics";
 import config from "../config";
 
-const Index = (props: any) => {
-  console.log(props);
-
-  return (
-    <div className="homePage">
-      <div>Welcome to next.js serverless ⚡</div>
-
-      <VictoryChart theme={theme} scale={{ x: "time", y: "linear" }}>
-        {/* <VictoryLine
-          style={{
-            data: { stroke: "#333", strokeWidth: 2 },
-            parent: { border: "1px solid #ccc" }
-          }}
-          data={props.data.datapoints.map((point: any) => ({
-            x: point.Timestamp,
-            y: point[statistic]
-          }))}
-          interpolation="bundle"
-        /> */}
-        <VictoryAxis crossAxis tickCount={5} />
-        {/* <VictoryAxis
-          dependentAxis
-          tickFormat={t => `${t}${suffix}`}
-          style={{
-            grid: { stroke: "#bbb" },
-            axis: { stroke: "transparent" }
-          }}
-        /> */}
-      </VictoryChart>
-    </div>
-  );
-};
+const Index = (props: any) => (
+  <div className="homePage">
+    {props.data.map((metric: MetricData) => (
+      <div>
+        <h2>{metric.title}</h2>
+        <h3>
+          {metric.datapoints[metric.datapoints.length - 1][metric.statistic]}
+          {metric.suffix}
+        </h3>
+        <VictoryChart theme={theme} scale={{ x: "time", y: "linear" }}>
+          <VictoryLine
+            style={{
+              data: { stroke: "#333", strokeWidth: 2 },
+              parent: { border: "1px solid #ccc" }
+            }}
+            data={metric.datapoints.map((point: any) => ({
+              x: point.Timestamp,
+              y: point[metric.statistic]
+            }))}
+            interpolation="bundle"
+          />
+          <VictoryAxis crossAxis tickCount={5} />
+          <VictoryAxis
+            dependentAxis
+            tickFormat={t => `${t}${metric.suffix}`}
+            style={{
+              grid: { stroke: "#bbb" },
+              axis: { stroke: "transparent" }
+            }}
+          />
+        </VictoryChart>
+      </div>
+    ))}
+  </div>
+);
 
 Index.getInitialProps = async function() {
   const data: MetricData[] = await getMetrics(config);
